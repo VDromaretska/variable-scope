141c2c78cbeed1fb9e8ec5f9e48320a1f16d57b6
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-06-18 14:31:27 +0200

    Fixed 1b

--- examples/1b-independent-function-scopes.js
+++ examples/1b-independent-function-scopes.js
@@ -1,11 +1,13 @@
 "use strict";
 
 const commonOuterScope = "sand";
+const prefix = "quick";
+const suffix = "castle";
 
 function printWithPrefix() {
   console.log('inside scope of function 1');
 
-  const prefix = "quick";
+  
   console.log("printing with prefix:", `${prefix}${commonOuterScope}`);
 
   // below line will error, comment out to fix
@@ -16,7 +18,7 @@ function printWithPrefix() {
 function printWithSuffix() {
   console.log("inside scope of function 2");
 
-  const suffix = "castle";
+  
   console.log('printing with suffix:', `${commonOuterScope}${suffix}`);
 
   // below line will error, comment out to fix
