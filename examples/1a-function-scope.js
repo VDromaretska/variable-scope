55eb9873b571d1ed15c8da9c313db92b87d5e8d0
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-06-18 14:28:15 +0200

    Fixed 1a

--- examples/1a-function-scope.js
+++ examples/1a-function-scope.js
@@ -4,14 +4,12 @@ const myOuterFruit = "apple";
 
 function printFruitNames() {
   const myInnerFruit = "pear";
+
+  console.log("Going to run my function:")
   console.log('a fruit defined in function scope:', myInnerFruit);
   console.log('a fruit defined in global scope:', myOuterFruit);
-}
 
-console.log("Going to run my function:")
-
+	console.log("outside a function, I can't access variables from that function's scope:", myInnerFruit); // causes an error! comment out this line to fix
+	console.log("outside a function, I can still access variables from the global scope", myOuterFruit);
+};
 printFruitNames();
-
-console.log("outside a function, I can't access variables from that function's scope:", myInnerFruit); // causes an error! comment out this line to fix
-
-console.log("outside a function, I can still access variables from the global scope", myOuterFruit);
